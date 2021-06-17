4b704b4a95f31f52183a7aa9f2e7ed61c81de318
Su Lai <sulaimyint@hotmail.com>
2021-06-17 22:18:22 +0800

    exam preparation

---
+++ MIU_CSharp_Class/MIUExam.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIU_CSharp_Class
+{
+    public class MIUExam
+    {
+        //testing class structure ...
+        public static int checkConcatenatedSum(int n, int catlen)
+        {
+            int num = n, sum = 0;
+
+            while (num > 0)
+            {
+                int digit = num % 10;
+                num /= 10;
+                for (int j = 1, i = 1; j <= catlen; j++)
+                {
+                    sum += digit * i;// 1st => 8 * 1 = 8, 2nd => 8 * 10 = 80
+                    i *= 10;
+                }
+            }
+            if (n == sum) return 1;
+            return 0;
+        }
+    }
+}
---
+++ MIU_CSharp_UnitTest/MIUExam/checkConcatenatedSum.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace MIU_CSharp_UnitTest.MIUExam
+{
+    [TestClass]
+    public class checkConcatenatedSum
+    {
+        [TestMethod]
+        public void checkConcatenatedSum1()
+        {
+            Assert.AreEqual(MIU_CSharp_Class.MIUExam.checkConcatenatedSum(198, 2), 1, "TEST 1");
+            Assert.AreEqual(MIU_CSharp_Class.MIUExam.checkConcatenatedSum(198, 3), 0, "TEST 2");
+        }
+    }
+}
